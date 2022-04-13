a17b35c83c111d98383edc02ce12f758e2db7d52
OkorieIfeanyichukwu <68703510+OkorieIfeanyichukwu@users.noreply.github.com>
2022-04-13 12:21:30 +0100

    first commit
    Trying out how to push vue app to github

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <navigationA/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to my first vue App github repo"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import navigationA from './components/navigationA.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    navigationA,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/navigationA.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>My name is Okorie Ifeanyi, i want to test how to push a vue app to github</h1>
+</template>
+
+
+<script>
+export default {
+    name:'navigationA'
+
+}
+</script>
+
+<style scoped>
+
+</style>
